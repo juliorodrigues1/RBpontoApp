b6851c38dd66d862f077ab42e608b98ff5632bf1
jose gabriel <gabriel2007ff@hotmail.com>
2021-10-13 09:12:09 -0500

    msg erro alterada

--- lib/controller/DownloadPdf.dart
+++ lib/controller/DownloadPdf.dart
@@ -59,9 +59,9 @@ Future<String> downloadFile(String url, String fileName, String dir) async {
       file = File(filePath);
       await file.writeAsBytes(bytes);
     } else
-      filePath = 'Error code: ' + response.statusCode.toString();
+      filePath = ' Error' + response.statusCode.toString();
   } catch (ex) {
-    filePath = 'Can not fetch url';
+    filePath = 'Url não encontrada';
   }
 
   return filePath;
