6be19557f2b74b729282c08cf6381d0b64410880
julio.cesar <julio.cesar@riobranco.ac.gov.br>
2021-11-08 12:12:47 -0500

    removendo arquivos

--- lib/values/preferences_keys.dart
+++
@@ -1,8 +0,0 @@
-class PreferencesKeys {
-  //KEY THE STORAGE LOCAL ACESS
-  static const activeUser = "LOGIN_USER_INFOS";
-  static const apiURL = "apiponto.riobranco.ac.gov.br";
-  static const apidemonstrativo= "http://apiponto.riobranco.ac.gov.br/api/demonstrativo/pdf";
-  static const apihomologa = "homologamac.riobranco.ac.gov.br";
-  static const homologadesmonstrativo = "http://homologamac.riobranco.ac.gov.br/api/demonstrativo/pdf";
-}
